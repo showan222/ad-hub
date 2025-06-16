9798b9868eba3c478af3b6167daf60ba6d44b28d
showan222 <showan7@yahoo.com>
2025-06-16 02:15:12 +0200

    Update frame.ts

--- pages/api/frame.ts
+++ pages/api/frame.ts
@@ -1,6 +1,17 @@
-// pages/api/frame.ts
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ ok: true })
+  res.status(200).json({
+    frame: {
+    
+      version: 'vNext',
+      image: 'https://ad-hub-ecru.vercel.app/cover.jpg',
+      post_url: 'https://ad-hub-ecru.vercel.app/api/frame',
+      buttons: [
+        {
+          label: 'Explore Projects'
+        }
+      ]
+    }
+  });
 }
