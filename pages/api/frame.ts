79078f6bb8223e4b254c0db47f156f14fa923978
showan222 <showan7@yahoo.com>
2025-06-16 02:52:13 +0200

    Update frame.ts
    import { NeynarAPIClient } from "@neynar/nodejs-sdk";
    import { FrameRequest, getFrameHtmlResponse } from "frames.js";
    
    const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
    
    export const config = {
      runtime: "edge",
    };
    
    export default async function handler(req: Request): Promise<Response> {
      const body = await req.json();
      const frameRequest = FrameRequest.parse(body);
    
      const userFid = frameRequest?.untrustedData?.fid;
    
      const html = getFrameHtmlResponse({
        buttons: [
          {
            label: "Visit GitHub",
            action: "link",
            target: "https://github.com/showan222/ad-hub",
          },
        ],
        image: "https://ad-hub-ecru.vercel.app/cover.png",
        postUrl: "https://ad-hub-ecru.vercel.app/api/frame",
        ogTitle: "Advantage Hub",
        ogDescription: `Verified Web3 projects — for you!`,
      });
    
      return new Response(html, {
        headers: {
          "Content-Type": "text/html",
        },
      });
    }

--- pages/api/frame.ts
+++ pages/api/frame.ts
@@ -1,17 +1,35 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NeynarAPIClient } from "@neynar/nodejs-sdk";
+import { FrameRequest, getFrameHtmlResponse } from "frames.js";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    frame: {
-    
-      version: 'vNext',
-      image: 'https://ad-hub-ecru.vercel.app/cover.jpg',
-      post_url: 'https://ad-hub-ecru.vercel.app/api/frame',
-      buttons: [
-        {
-          label: 'Explore Projects'
-        }
-      ]
-    }
+const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req: Request): Promise<Response> {
+  const body = await req.json();
+  const frameRequest = FrameRequest.parse(body);
+
+  const userFid = frameRequest?.untrustedData?.fid;
+
+  const html = getFrameHtmlResponse({
+    buttons: [
+      {
+        label: "Visit GitHub",
+        action: "link",
+        target: "https://github.com/showan222/ad-hub",
+      },
+    ],
+    image: "https://ad-hub-ecru.vercel.app/cover.png",
+    postUrl: "https://ad-hub-ecru.vercel.app/api/frame",
+    ogTitle: "Advantage Hub",
+    ogDescription: `Verified Web3 projects — for you!`,
+  });
+
+  return new Response(html, {
+    headers: {
+      "Content-Type": "text/html",
+    },
   });
 }
